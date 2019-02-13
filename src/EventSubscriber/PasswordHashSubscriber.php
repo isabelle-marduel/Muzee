a7d2d52e7174034454c62412017e7b71ad9ed369
Isabelle Marduel <isabelle.marduel@gmail.com>
2019-02-13 10:53:32 +0100

    eventSubscriber

---
+++ src/EventSubscriber/PasswordHashSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventSubcriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class PasswordHashSubscriber implements EventSubscriberInterface
+{
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function hashPassword(GetResponseForControllerResultEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || Request::METHOD_POST !== $method){
+            return;
+        }
+
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $user->getPassword())
+        );
+    }
+}
